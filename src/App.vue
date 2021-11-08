52db7b5f68b99edb3d672b909249bf1fb27a61e3
theodtasia <theodoratasia@gmail.com>
2021-11-08 14:09:21 +0200

    Add layout and menu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-dark h-screen">
+    <div class="flex" style="height: 88vh">
+      <div class="w-56 bg-black h-full flex-none">
+        <div class="p-6">
+          <img src="./assets/spotify.png" class="h-10" />
+        </div>
+        <ul id="apage" class="mx-2">
+          <li
+            v-for="(page, index) in pages"
+            :key="page.id"
+            :class="[
+              'w - full',
+              'h - full',
+              'justify-center',
+              'p-3',
+              'text-lightest',
+            ]"
+          >
+            <font-awesome-icon
+              :icon="['fa', page.icon]"
+              class="fa-lg icon alt mx-4"
+            />
+            <button @click="(setID = page.id), setPage(index)">
+              <p>{{ page.name }}</p>
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div class="w-full h-full relative bg-blue-400">
+        <div class="w-full sticky top-0 bg-yellow-500 p-2"></div>
+      </div>
+    </div>
+
+    <div class="w-full bg-light" style="height: 12vh"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      pages: [
+        { id: "home", name: "Home", icon: "home" },
+        { id: "search", name: "Search", icon: "search" },
+        { id: "library", name: "Library", icon: "music" },
+      ],
+      setID: "home",
+    };
+  },
+  methods: {
+    async setPage(index) {
+      for (var i = 0; i < this.pages.length; i++) {
+        if (i == index) {
+          var li = document.getElementById("apage").querySelectorAll("li")[
+            index
+          ];
+          li.classList.add("bg-light");
+          li.classList.remove("text-lightest");
+          li.classList.add("text-white");
+        } else {
+          li = document.getElementById("apage").querySelectorAll("li")[i];
+          li.classList.remove("bg-light");
+          li.classList.remove("text-white");
+          li.classList.add("text-lightest");
+        }
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/css/tailwind.css";
+Vue.config.productionTip = false;
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faHome, faSearch, faMusic } from "@fortawesome/free-solid-svg-icons";
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+library.add(faSearch, faHome, faMusic);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
